1e60008ad5dfcf3e08fcb7815302f80ee05dfe9e
miltonhuynh <miltonhuynh@gmail.com>
2022-05-19 21:00:15 -0700

    Added Binary Search Page

--- app/src/Components/Content.js
+++ app/src/Components/Content.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
 
 export default function Content() {
   return (
     <div id="Content">
         CONTENT PLACEHOLDER
+        <Link to="/DSA-guide/BinarySearch">Invoices</Link> 
+        <Outlet/>
     </div>
   )
 }
--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BinarySearch from './Algorithms/BinarySearch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <App />
+      <Route path="/DSA-guide" element={<App />}>
+        <Route path="/DSA-guide/BinarySearch" element={<BinarySearch />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
