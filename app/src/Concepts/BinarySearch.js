bcf5d491088ebab3335861093d82b6b3809e8360
miltonhuynh <miltonhuynh@gmail.com>
2022-05-21 21:55:43 -0700

    Added prism to display code

--- app/src/Concepts/BinarySearch.js
+++ app/src/Concepts/BinarySearch.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Prism from "prismjs";
+import "../prism/prism.css"
+
 
 export default function BinarySearch() {
   return (
     <div>
-      BinarySearch placeholder
-      <code>Test</code>
+      <pre><code class="language-js">console.log("Hello World")</code></pre>
     </div>
   )
 }
