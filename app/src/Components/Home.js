4f48d003888affb9e8725a4cb321cc5125bf1895
miltonhuynh <miltonhuynh@gmail.com>
2022-05-25 20:09:38 -0700

    Added disclaimer message

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Home() {
   return (
     <div class="Concept_page">
-      <div class="Concept_description">This website is still under construction, feel free to visit every now and then to check my progress!</div>
+      <div class="Concept_description">
+        <div>This website is still under construction, feel free to visit every now and then to check my progress!</div><br></br>
+        <div>Disclaimer: This is just a quick reference, please check out other resources to learn more in-depth! </div>
+      </div>
     </div>
   )
 }
