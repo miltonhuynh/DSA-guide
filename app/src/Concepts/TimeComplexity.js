7bed991650150fffca73d7212beb327e2de28d83
miltonhuynh <miltonhuynh@gmail.com>
2022-05-31 21:07:49 -0700

    Added code box to time complexity page

--- app/src/Concepts/TimeComplexity.js
+++ app/src/Concepts/TimeComplexity.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+
+const slow_python_snippet = 
+`function addUpTo(n) {
+  let total = 0;
+}`
 
 export default function BigO() {
   return (
@@ -8,6 +16,42 @@ export default function BigO() {
         Time complexity describes how long it takes a computer to run an algorithm. There are many ways to 
         solve a problem but some methods will take less time than others. It is important to consider
         run time as the amount of data grows so will how long it will take the function to return an answer.
+        <br /><br />
+        For example, consider a function that adds all the numbers from 1 to some number n.
+        <br /><br />
+        Method 1:
+        <br />
+        <div class="Code_box">
+        <Tabs>
+          <TabList>
+            <Tab class="Tab_title">Python</Tab>
+            <Tab class="Tab_title">C++</Tab>
+            <Tab class="Tab_title">Java</Tab>
+            <Tab class="Tab_title">JavaScript</Tab>
+          </TabList>
+
+          <TabPanel>
+            <div class="Code">
+                <pre><code>{slow_python_snippet}</code></pre>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div class="Code">
+                <pre><code>{slow_python_snippet}</code></pre>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div class="Code">
+                <pre><code>{slow_python_snippet}</code></pre>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div class="Code">
+                <pre><code>{slow_python_snippet}</code></pre>
+            </div>
+          </TabPanel>
+        </Tabs>
+      </div>
       </div>
 
     </div>
