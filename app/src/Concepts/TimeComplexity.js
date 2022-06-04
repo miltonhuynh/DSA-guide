3f692de4ba48cc3db02bf2b060e7ca127e110672
miltonhuynh <miltonhuynh@gmail.com>
2022-06-03 22:21:55 -0700

    Positioning time complexity

--- app/src/Concepts/TimeComplexity.js
+++ app/src/Concepts/TimeComplexity.js
@@ -51,7 +51,7 @@ export default function BigO() {
 
         <h3>O(1)</h3>
         <h4>Constant time as n scales</h4>
-        <h5>Example</h5>
+        <h5 style={{position: "relative", left: "2vw"}}>Example</h5>
         <pre>
           <code class="Code_box Code">
             x = 5;
