53468a992f903b276d8bbc38109e98ddf252c81c
miltonhuynh <miltonhuynh@gmail.com>
2022-06-07 19:37:44 -0700

    Clicking on site title now goes to home

--- app/src/Components/HeaderFooter.js
+++ app/src/Components/HeaderFooter.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ReactComponent as Githubicon } from '../github-brands.svg';
+import { useNavigate } from 'react-router-dom'
 
 export default function HeaderFooter(props) {
+  let navigate = useNavigate();
+
   return (
     <>
         <div id="header">
           <div id="Github-icon"><a href="https://github.com/miltonhuynh/DSA-guide"><Githubicon/></a></div>
-          Milton's Data Structures and Algorithms Guide
+          <div id="Brand" onClick={() => navigate("")}>Milton's Data Structures and Algorithms Guide</div>
         </div>
             {props.children}
         <div id="footer">Created by Milton Huynh, with React.js</div>
--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div class="Concept_page">
+      <div class="Concept_title">Welcome!</div>
       <div class="Concept_description">
         <div>This website is still under construction, feel free to visit every now and then to check my progress!</div><br></br>
         <div>Disclaimer: This is just a quick reference, please check out other resources to learn more in-depth! </div>
