f1db22be36742cee50d21d61b659d7258ea75d31
miltonhuynh <miltonhuynh@gmail.com>
2022-06-09 22:38:20 -0700

    Modified frequency count

--- app/src/Concepts/FrequencyCounter.js
+++ app/src/Concepts/FrequencyCounter.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function FrequencyCounter() {
   return (
     <div class="Concept_page">
-    <div class="Concept_title">Frequency Counter</div>
-  </div>
+      <div class="Concept_title">Frequency Counter</div>
+      <div class="Concept_description">
+        <div>This pattern uses objects or sets to collect values and frequencies of values.</div>
+        <div>Used to avoid using nested loops or O(n^2) with arrays and strings</div>
+      </div>
+    </div>
   )
 }
