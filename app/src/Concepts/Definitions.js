b3e541d9394cf5da74c3942edd9def8bfe3ea87a
miltonhuynh <miltonhuynh@gmail.com>
2022-06-11 09:41:50 -0700

    Added hash map term

--- app/src/Concepts/Definitions.js
+++ app/src/Concepts/Definitions.js
@@ -7,6 +7,7 @@ export default function Definitions() {
       <div id="Terms_list">
           <div class="Term"><u>Abstract Data type</u>: a type (or class) for objects whose behaviour is defined by a set of value and a set of operations.</div>
           <div class="Term"><u>Data structure</u>: A way of organizing data so that it can be used effectively.</div>
+          <div class="Term"><u>Hashmap:</u> a way to store some data and retrieve it based off of a particular key.</div>
           <div class="Term"><u>Node:</u> a basic unit of a data structure, such as a linked list or tree data structure. Nodes contain data and also may link to other nodes.</div>
       </div>
     </div>
