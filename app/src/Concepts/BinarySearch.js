52d73cee067edc67fac2dcb41df8f5dca29a3aa7
miltonhuynh <miltonhuynh@gmail.com>
2022-06-12 22:00:53 -0700

    Added implementation for frequency counter pattern

--- app/src/Concepts/BinarySearch.js
+++ app/src/Concepts/BinarySearch.js
@@ -36,14 +36,12 @@ const javascript_snippet =
 
 
   return (
-    <div class="Concept_page">
-      <div class="Concept_title">Binary Search</div>
-      <div class="Container font-m">
-        <div>Binary search is an efficient algorithm for finding an item from a sorted list of items. It works by repeatedly dividing in half the portion of the list that could contain the item, until you've narrowed down the possible locations to just one. We used binary search in the guessing game in the introductory tutorial. One of the most common ways to use binary search is to find an item in an array. For example, the Tycho-2 star catalog contains information about the brightest 2,539,913 stars in our galaxy. Suppose that you want to search the catalog for a particular star, based on the star's name. If the program examined every star in the star catalog in order starting with the first, an algorithm called linear search, the computer might have to examine all 2,539,913 stars to find the star you were looking for, in the worst case. If the catalog were sorted alphabetically by star names, binary search would not have to examine more than 22 stars, even in the worst case.</div>
-        <div class="Code_box">
-          <div class="Code">
-              <pre><code>{javascript_snippet}</code></pre>
-          </div>
+  <div class="Concept_page">
+    <div class="Concept_title">Binary Search</div>
+    <div class="Container font-m">
+      <div>Binary search is an efficient algorithm for finding an item from a sorted list of items. It works by repeatedly dividing in half the portion of the list that could contain the item, until you've narrowed down the possible locations to just one. We used binary search in the guessing game in the introductory tutorial. One of the most common ways to use binary search is to find an item in an array. For example, the Tycho-2 star catalog contains information about the brightest 2,539,913 stars in our galaxy. Suppose that you want to search the catalog for a particular star, based on the star's name. If the program examined every star in the star catalog in order starting with the first, an algorithm called linear search, the computer might have to examine all 2,539,913 stars to find the star you were looking for, in the worst case. If the catalog were sorted alphabetically by star names, binary search would not have to examine more than 22 stars, even in the worst case.</div>
+        <div class="Code">
+            <pre><code>{javascript_snippet}</code></pre>
         </div>
       </div>
     </div>
--- app/src/Concepts/FrequencyCounter.js
+++ app/src/Concepts/FrequencyCounter.js
@@ -1,12 +1,72 @@
 import React from 'react'
 
 export default function FrequencyCounter() {
+
+  const naive = 
+  `function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    for(let i = 0; i < arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1) {
+            return false;
+        }
+        console.log(arr2);
+        arr2.splice(correctIndex,1)
+    }
+    return true;
+}`
+
+  const refactored =
+  `function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    return true
+}`
+
+
   return (
     <div class="Concept_page">
       <div class="Concept_title">Frequency Counter</div>
-      <div class="Container font-m">
-        <div>This pattern uses objects or sets to collect values and frequencies of values.</div>
-        <div>Used to avoid using nested loops or O(n^2) with arrays and strings</div>
+      <div class="Container">
+        <div class="font-m">This pattern uses objects or sets to collect values and frequencies of values.</div><br/>
+        <div class="font-m">Used to avoid using nested loops or O(n^2) with arrays and strings</div><br/>
+        <div class="font-l">Naive solution</div>
+        <div class="Code">
+          <pre>
+            <code>
+              {naive}
+            </code>
+          </pre>
+        </div>
+        <div class="font-l">Optimal solution</div>
+        <div class="Code">
+          <pre>
+            <code>
+              {refactored}
+            </code>
+          </pre>
+        </div>
       </div>
     </div>
   )
