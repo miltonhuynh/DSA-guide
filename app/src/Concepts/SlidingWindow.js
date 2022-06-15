03271dfb19ff0f14f5357d0a0d0b422fe394633e
miltonhuynh <miltonhuynh@gmail.com>
2022-06-14 20:33:50 -0700

    added sliding window description

--- app/src/Concepts/SlidingWindow.js
+++ app/src/Concepts/SlidingWindow.js
@@ -4,6 +4,13 @@ export default function SlidingWindow() {
   return (
     <div class="Concept_page">
     <div class="Concept_title">Sliding Window</div>
+    <div class="Container">
+      <div class="font-m">
+        This pattern involves creating a window which can either be an array or number from one position to another.<br/><br/>
+        Depending on a certain condition, the window either increases or closes and a new window is created.<br/><br/>
+        Very useful for keeping track of a subset of data in an array/string etc.
+      </div>
+    </div>
   </div>
   )
 }
