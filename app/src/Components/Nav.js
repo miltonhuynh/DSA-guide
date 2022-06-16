6edda9124bc5b18a618635600fcb8d332d4a2353
miltonhuynh <miltonhuynh@gmail.com>
2022-06-15 17:30:27 -0700

    Added radix sort page

--- app/src/Components/Nav.js
+++ app/src/Components/Nav.js
@@ -6,26 +6,27 @@ export default function Nav() {
 
   return (
     <div id="Navigator">
-      <div class="Concept_Links"  onClick={() => navigate("")}>Welcome Page</div>
-      <div class="Concept_Links"  onClick={() => navigate("Definitions")}>Common Terms</div>
-      <div class="Concept_Links" onClick={() => navigate("TimeComplexity")}>Time Complexity</div>
+      <div class="Concept_Links" onClick={() => navigate("")}>Welcome Page</div>
+      <div class="Concept_Links" onClick={() => navigate("BinarySearch")}>Binary Search</div>
+      <div class="Concept_Links" onClick={() => navigate("BreadthFirstSearch")}>Breadth First Search</div>
+      <div class="Concept_Links" onClick={() => navigate("BubbleSort")}>Bubble Sort</div>
+      <div class="Concept_Links" onClick={() => navigate("Definitions")}>Common Terms</div>
+      <div class="Concept_Links" onClick={() => navigate("DepthFirstSearch")}>Depth First Search</div>
+      <div class="Concept_Links" onClick={() => navigate("Dijkstra")}>Dijkstra Algorithm</div>
+      <div class="Concept_Links" onClick={() => navigate("DivideConquer")}>Divide and Conquer</div>
+      <div class="Concept_Links" onClick={() => navigate("FrequencyCounter")}>Frequency Counter</div>
+      <div class="Concept_Links" onClick={() => navigate("HashTables")}>Hash Tables</div>
+      <div class="Concept_Links" onClick={() => navigate("HeapSort")}>Heap Sort</div>
+      <div class="Concept_Links" onClick={() => navigate("MergeSort")}>Merge Sort</div>
+      <div class="Concept_Links" onClick={() => navigate("InsertionSort")}>Insertion Sort</div>
+      <div class="Concept_Links" onClick={() => navigate("PriorityQueue")}>Priority Queue</div>
+      <div class="Concept_Links" onClick={() => navigate("QuickSort")}>Quick Sort</div>
+      <div class="Concept_Links" onClick={() => navigate("Recursion")}>Recursion</div>
+      <div class="Concept_Links" onClick={() => navigate("SelectionSort")}>Selection Sort</div>
+      <div class="Concept_Links" onClick={() => navigate("SlidingWindow")}>Sliding Window</div>
       <div class="Concept_Links" onClick={() => navigate("SpaceComplexity")}>Space Complexity</div>
       <div class="Concept_Links" onClick={() => navigate("Stack")}>Stack</div>
-      <div class="Concept_Links" onClick={() => navigate("Recursion")}>Recursion</div>
-      <div class="Concept_Links" onClick={() => navigate("SlidingWindow")}>Sliding Window</div>
-      <div class="Concept_Links" onClick={() => navigate("FrequencyCounter")}>Frequency Counter</div>
-      <div class="Concept_Links" onClick={() => navigate("BinarySearch")}>Binary Search</div>
-      <div class="Concept_Links"  onClick={() => navigate("BubbleSort")}>Bubble Sort</div>
-      <div class="Concept_Links"  onClick={() => navigate("QuickSort")}>Quick Sort</div>
-      <div class="Concept_Links"  onClick={() => navigate("MergeSort")}>Merge Sort</div>
-      <div class="Concept_Links"  onClick={() => navigate("HeapSort")}>Heap Sort</div>
-      <div class="Concept_Links"  onClick={() => navigate("SelectionSort")}>Selection Sort</div>
-      <div class="Concept_Links"  onClick={() => navigate("InsertionSort")}>Insertion Sort</div>
-      <div class="Concept_Links"  onClick={() => navigate("HashTables")}>Hash Tables</div>
-      <div class="Concept_Links"  onClick={() => navigate("BreadthFirstSearch")}>Breadth First Search</div>
-      <div class="Concept_Links"  onClick={() => navigate("DepthFirstSearch")}>Depth First Search</div>
-      <div class="Concept_Links"  onClick={() => navigate("Dijkstra")}>Dijkstra Algorithm</div>
-      <div class="Concept_Links"  onClick={() => navigate("PriorityQueue")}>Priority Queue</div>
+      <div class="Concept_Links" onClick={() => navigate("TimeComplexity")}>Time Complexity</div>
     </div>
   )
 }
---
+++ app/src/Concepts/DivideConquer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DivideConquer() {
+  return (
+    <div class="Concept_page">
+      <div class="Concept_title">Divide and Conquer</div>
+    </div>
+  )
+}
