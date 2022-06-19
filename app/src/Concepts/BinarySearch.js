b364a7df707925a4f594621f376b263ffc0a02dd
miltonhuynh <miltonhuynh@gmail.com>
2022-06-18 21:17:38 -0700

    Edited binary search code

--- app/src/Concepts/BinarySearch.js
+++ app/src/Concepts/BinarySearch.js
@@ -5,29 +5,23 @@ export default function BinarySearch() {
   
 
 const javascript_snippet = 
-  `function binarySearch(arr, l, r, x) {
-    if (r >= l) {
-      let mid = l + Math.floor((r - l) / 2);
-  
-      if (arr[mid] == x)
-        return mid;
-
-      if (arr[mid] > x)
-        return binarySearch(arr, l, mid - 1, x);
-
-      return binarySearch(arr, mid + 1, r, x);
+  `function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]){
+            end = middle;
+        } else {
+            start = middle;
+        }
+        middle = Math.floor((start + end) / 2);
     }
-  
-
+    if(arr[middle] === elem){
+        return middle;
+    }
     return -1;
-  }
-  
-  let arr = [ 2, 3, 4, 10, 40 ];
-  let x = 10;
-  let n = arr.length
-  let result = binarySearch(arr, 0, n - 1, x);
-  (result == -1) ? document.write( "Element is not present in array")
-          : document.write("Element is present at index " +result);`
+}`
 
 
 
