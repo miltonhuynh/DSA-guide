117a3ab8cd1d652eac356764b526e4b1cda7ca0b
miltonhuynh <miltonhuynh@gmail.com>
2022-06-20 12:48:51 -0700

    Added sorting definition

--- app/src/Concepts/Definitions.js
+++ app/src/Concepts/Definitions.js
@@ -9,6 +9,8 @@ export default function Definitions() {
           <div class="Term"><u>Data structure</u>: A way of organizing data so that it can be used effectively.</div>
           <div class="Term"><u>Hashmap:</u> a way to store some data and retrieve it based off of a particular key.</div>
           <div class="Term"><u>Node:</u> a basic unit of a data structure, such as a linked list or tree data structure. Nodes contain data and also may link to other nodes.</div>
+          <div class="Term"><u>Sorting:</u> The process of rearranging the items in a collection (e.g. an array) so that items are in some kind of order.</div>
+nodes
       </div>
     </div>
   )
