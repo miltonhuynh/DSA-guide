44cd75cdbf671d87afe6c87268c033663b755927
miltonhuynh <miltonhuynh@gmail.com>
2022-06-23 16:51:47 -0700

    Uncommited changes

--- app/src/Concepts/BubbleSort.js
+++ app/src/Concepts/BubbleSort.js
@@ -4,6 +4,9 @@ export default function BubbleSort() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Bubble Sort</div>
+      <div class="Container">
+        
+      </div>
     </div>
   )
 }
