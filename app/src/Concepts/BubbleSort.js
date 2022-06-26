08590d8400301e428b84d04df4802021c8d991da
miltonhuynh <miltonhuynh@gmail.com>
2022-06-26 10:04:02 -0700

    test commit

--- app/src/Concepts/BubbleSort.js
+++ app/src/Concepts/BubbleSort.js
@@ -26,10 +26,12 @@ export default function BubbleSort() {
             <li><div>This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high.</div></li><br/>
             <li><div>With each iteration, the largest number "bubbles" to the top.</div></li><br/>
             <li><div>Implementation steps:</div></li>
-            <div class="indent">1) Start looping with a variable called i at the end of the array towards the beginning.</div>
-            <div class="indent">2) Start an inner loop with a variable called j from the beginning until i - 1</div>
-            <div class="indent">3) If arr[j] is greater than arr[j+1], swap those two values!</div>
-            <div class="indent">4) Return the sorted array.</div>
+            <div class="indent font-sm">
+              <div>1) Start looping with a variable called i at the end of the array towards the beginning.</div>
+              <div>2) Start an inner loop with a variable called j from the beginning until i - 1</div>
+              <div>3) If arr[j] is greater than arr[j+1], swap those two values!</div>
+              <div>4) Return the sorted array.</div>
+            </div>
           </ul>
         </div>
       <div class="font-l">Implementation</div>
--- app/src/Concepts/InsertionSort.js
+++ app/src/Concepts/InsertionSort.js
@@ -4,7 +4,21 @@ export default function InsertionSort() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Insertion sort</div>
-      
+      <div class="Container">
+        <div class="font-m">
+          <ul>
+            <li><div>Insertion sort works like organizing playing cards in your hands.</div></li><br/>
+            <li><div>Builds up the sort by gradually creating a larger left half which is always sorted.</div></li><br/>
+            <li><div>Implementation steps:</div></li>
+            <div class="indent font-sm">
+              <div>1) Start by picking the second element in the array.</div>
+              <div>2) Compare the second element with the one before it and swap if necessary.</div>
+              <div>3) Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place.</div>
+              <div>4) Repeat until the array is sorted.</div>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
