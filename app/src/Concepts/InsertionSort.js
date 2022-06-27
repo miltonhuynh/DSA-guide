5c9b3ed859088a640951f050940bab3d228759e1
miltonhuynh <miltonhuynh@gmail.com>
2022-06-26 20:55:45 -0700

    Started insertion sort code box

--- app/src/Concepts/InsertionSort.js
+++ app/src/Concepts/InsertionSort.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function InsertionSort() {
+
+  const insertion = 
+  `a`
   return (
     <div class="Concept_page">
       <div class="Concept_title">Insertion sort</div>
@@ -15,6 +18,13 @@ export default function InsertionSort() {
               <div>2) Compare the second element with the one before it and swap if necessary.</div>
               <div>3) Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place.</div>
               <div>4) Repeat until the array is sorted.</div>
+            </div>
+            <div class="Code">
+                <pre>
+                  <code>
+                    {insertion}
+                  </code>
+                </pre>
             </div>
           </ul>
         </div>
