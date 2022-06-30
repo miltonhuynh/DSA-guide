0ddc4a39c4b26e4abe4de5e3552eb25defa27b76
miltonhuynh <miltonhuynh@gmail.com>
2022-06-29 20:55:39 -0700

    Added bullet point for insertion sort

--- app/src/Concepts/InsertionSort.js
+++ app/src/Concepts/InsertionSort.js
@@ -24,6 +24,7 @@ export default function InsertionSort() {
             <li><div>Insertion sort works like organizing playing cards in your hands.</div></li><br/>
             <li><div>Builds up the sort by gradually creating a larger left half which is always sorted.</div></li><br/>
             <li><div>Insertion sort is adaptive in nature, i.e. it is appropriate for data sets which are already partially sorted.</div></li><br/>
+            <li><div>If data is almost sorted, time complexity is better than bubble sort and selection sort.</div></li><br/>
             <li><div>Implementation steps:</div></li>
             <div class="indent font-sm">
               <div>1) Start by picking the second element in the array.</div>
