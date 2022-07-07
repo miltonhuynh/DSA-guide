ca7ca2ac3e12562d2f10c3e37a211aa2702c1f77
miltonhuynh <miltonhuynh@gmail.com>
2022-07-07 14:05:23 -0700

    Modified home page

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
         <div>This website is still under construction, feel free to visit every now and then to check my progress!</div><br></br>
         <div>Data Structures and Algorithms is the way data is organized and manipulated. This topic shows up on interviews a lot and the concepts will be valuable throughout a developer's career.</div><br/>
         <div>This topic can be challenging and I created this website to help myself and others break these concepts down and understand them better.</div><br/>
+        <div>This site is designed to be a review and crash course rather than a complete beginner's guide, please visit other resources for more thourough explanations!</div>
         <div>If you have any questions, comments or suggestions, please contact me at miltonhuynh@gmail.com</div>
       </div>
     </div>
