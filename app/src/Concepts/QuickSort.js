c956140c363da49487814ef63649c672775f663d
miltonhuynh <miltonhuynh@gmail.com>
2022-07-07 16:37:42 -0700

    Added quick sort implementation steps

--- app/src/Concepts/QuickSort.js
+++ app/src/Concepts/QuickSort.js
@@ -10,6 +10,13 @@ export default function QuickSort() {
             <li>Works by selecting one element (the pivot) and finding the index where the pivot should end up in the sorted array.</li><br/>
             <li>Counts the number of elements on each side of the pivot.</li><br/>
             <li>Once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot.</li><br/>
+            <li>Implementation Steps:</li>
+            <div class="indent font-sm">
+              <div>1) Choose an element in the array to be the pivot.</div>
+              <div>2) Iterate through the array and find the index where the pivot should be, swap so all numbers less than pivot is on the left side of the array and greater than to the right.</div>
+              <div>3) Swap the pivot element with the element in its correct index.</div>
+              <div>4) Recursively repeat process until the array is sorted.</div>
+            </div>
           </div>
         </ul>
       </div>
