c32c04975908d7fbf8ffb9e667f8b4475199aed6
miltonhuynh <miltonhuynh@gmail.com>
2022-07-10 20:39:47 -0700

    spelling

--- app/src/Concepts/MergeSort.js
+++ app/src/Concepts/MergeSort.js
@@ -32,7 +32,7 @@ function mergeSort(arr){
     let mid = Math.floor(arr.length/2);
     let left = mergeSort(arr.slice(0,mid));
     let right = mergeSort(arr.slice(mid));
-    return merge(left, sright);
+    return merge(left, right);
 }`
 
   return (
