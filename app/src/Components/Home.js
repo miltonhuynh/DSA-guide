3b4d6e406bb77f54c52734ad8932d7bc618830b0
miltonhuynh <miltonhuynh@gmail.com>
2022-07-12 17:44:28 -0700

    Modified home page, edited radix sort

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div class="Container font-m">
         <div>This website is still under construction, feel free to visit every now and then to check my progress!</div><br></br>
         <div>Data structures and algorithms can be challenging and I created this website to help myself and others break these concepts down and understand them better.</div><br/>
-        <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thourough explanations!</div>
+        <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thourough explanations!</div><br/>
         <div>If you have any questions, comments or suggestions, please contact me at miltonhuynh@gmail.com</div>
       </div>
     </div>
--- app/src/Concepts/RadixSort.js
+++ app/src/Concepts/RadixSort.js
@@ -4,6 +4,14 @@ export default function RadixSort() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Radix Sort</div>
+      <div class="Container">
+        <div class="font-m">
+            <ul>
+                <li>Radix sort is a special sorting algorithm that works on lists of numbers</li><br/>
+                <li>Never makes comparisons between elements</li><br/>
+            </ul>
+        </div>
+        </div>
     </div>
   )
 }
