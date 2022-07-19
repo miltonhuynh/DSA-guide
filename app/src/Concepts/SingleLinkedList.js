78de6c0fb0fe630043b1907d18533f734b3019b7
miltonhuynh <miltonhuynh@gmail.com>
2022-07-18 17:37:00 -0700

    Added singleLL container

--- app/src/Concepts/SingleLinkedList.js
+++ app/src/Concepts/SingleLinkedList.js
@@ -4,6 +4,13 @@ export default function SingleLinkedList() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Single Linked List</div>
+      <div class="Container">
+        <ul>
+          <div class="font-m">
+            <li>test</li>
+          </div>
+        </ul>
+      </div>
     </div>
   )
 }
