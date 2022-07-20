b03101f9768143805f78785220c34fa5120e3648
miltonhuynh <miltonhuynh@gmail.com>
2022-07-19 17:26:26 -0700

    Added more info for SLL and image

--- app/src/Concepts/SinglyLinkedList.js
+++ app/src/Concepts/SinglyLinkedList.js
@@ -7,9 +7,12 @@ export default function SinglyLinkedList() {
       <div class="Container">
         <ul>
           <div class="font-m">
-            <li>Each node points to the next item, unlike doubly linked list which points to the previous and next item.</li>
+            <li>Each node points to the next item, unlike doubly linked list which points to the previous and next item.</li><br/>
+            <li>Does not have indexes, connected via nodes with a next pointer, random access is not allowed.</li><br/>
+            <li>Unlike arrays, insertion and deletion is not as time consuming, don't need to re-index every element.</li><br/>
           </div>
         </ul>
+        <img src={require('../images/sll.png')} style={{width: "60vw"}} alt="Singly linked list illustration"></img>
       </div>
     </div>
   )
