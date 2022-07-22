e6d9c4aab2d153e5f469cffc1a98f35e80ca7032
miltonhuynh <miltonhuynh@gmail.com>
2022-07-21 17:47:04 -0700

    Added explanation of push and pop to SLL

--- app/src/Concepts/SinglyLinkedList.js
+++ app/src/Concepts/SinglyLinkedList.js
@@ -16,17 +16,26 @@ export default function SinglyLinkedList() {
               <div class="indent font-sm">1) Create a node that contains a variable that holds a value and a pointer to the next node.</div>
             </div>
             <div class="indent">
-              <div>Push</div>
-              <div class="indent font-sm">2) Let this function accept a value</div>
-              <div class="indent font-sm">3) Create a new node using the value passed to the function</div>
-              <div class="indent font-sm">4) If there is no head property on the list, set the head and tail to be the newly created node</div>
-              <div class="indent font-sm">5) Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node</div>
-              <div class="indent font-sm">6) Increment the length by 1</div>
-              <div class="indent font-sm">7) Return the linked list</div>
+              <div>Push (Adding a node to the beginning of list)</div>
+                <div class="indent font-sm">
+                <div>2) Let this function accept a value</div>
+                <div>3) Create a new node using the value passed to the function</div>
+                <div>4) If there is no head property on the list, set the head and tail to be the newly created node</div>
+                <div>5) Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node</div>
+                <div>6) Increment the length by 1</div>
+                <div>7) Return the linked list</div>
+              </div>
             </div>
             <div class="indent">
-              <div>Pop</div>
-              <div class="indent font-sm">1)</div>
+              <div>Pop (Removing a node from the end of the list)</div>
+                <div class="indent font-sm">
+                  <div>8) If there are no nodes in the list, return undefined</div>
+                  <div>9) Loop through the list until you reach the tail</div>
+                  <div>10) Set the next property of the 2nd to last node to be null</div>
+                  <div>11) Set the tail to be the 2nd to last node</div>
+                  <div>12) Decrement the length of the list by 1</div>
+                  <div>13) Return the value of the node removed</div>
+                </div>
             </div>
           </div>
         </ul>
