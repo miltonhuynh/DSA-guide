1f783805ed992caf30bde90b3527ccd694135875
miltonhuynh <miltonhuynh@gmail.com>
2022-08-02 19:25:27 -0700

    Removed period in DLL

--- app/src/Concepts/DoublyLinkedList.js
+++ app/src/Concepts/DoublyLinkedList.js
@@ -9,7 +9,7 @@ export default function DoublyLinkedList() {
           <div class="font-m">
             <li>Almost identical to Singly Linked Lists, except every node has another pointer to the previous node</li>
             <li>Can be traversed in both forward and backward directions, can quickly remove or add a new node before a given node</li>
-            <li>Every node of requires extra space for a previous pointer.</li>
+            <li>Every node of requires extra space for a previous pointer</li>
           </div>
         </ul>
       </div>
