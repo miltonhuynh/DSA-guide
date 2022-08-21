b63db092232e2978c4c71d61908964e4183e4a91
miltonhuynh <miltonhuynh@gmail.com>
2022-08-21 09:46:27 -0700

    changed text on home page

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div class="Concept_page">
       <div class="Concept_title">Welcome!</div>
       <div class="Container font-m">
-        <div>This website is still under construction, so expect some pages to look incomplete, please visit every now and then to check my progress!</div><br></br>
+        <div>This website is still under construction, so expect some pages to look incomplete</div><br></br>
         <div>Data structures and algorithms can be challenging and I created this website to help myself and others break these concepts down and understand them better.</div><br/>
         <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thourough explanations!</div><br/>
       </div>
