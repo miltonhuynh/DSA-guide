6712b7ee4083ccd0fc63c684611dd9d07323947a
miltonhuynh <miltonhuynh@gmail.com>
2022-08-23 20:40:42 -0700

    Added DFS implementation and code for all 3 variations

--- app/src/Concepts/DepthFirstSearch.js
+++ app/src/Concepts/DepthFirstSearch.js
@@ -3,11 +3,35 @@ import React from 'react'
 export default function DepthFirstSearch() {
 
   const preorder =
-  `DFSPreOrder(){
+  `DFSPreOrder() {
     var data = [];
-    function traverse(node){
+    function traverse(node) {
         data.push(node.value);
         if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+}`
+
+  const postorder =
+  `DFSPostOrder(){
+    var data = [];
+    function traverse(node){
+        if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+        data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+}`
+
+  const inorder =
+  `DFSInOrder(){
+    var data = [];
+    function traverse(node){
+        if(node.left) traverse(node.left);
+        data.push(node.value);
         if(node.right) traverse(node.right);
     }
     traverse(this.root);
@@ -31,7 +55,8 @@ export default function DepthFirstSearch() {
           <ul class="font-sm">
             <li>Visits the root first</li>
             <li>Traverses the left subtree and then the right subtree</li>
-            <li>Commonly used to create a copy of the tree</li>
+            <li>Useful for searching for an element within a tree</li><br/>
+            <img src={require('../images/DFSpreorder.png')} style={{width: "30vw"}} alt="DFS pre-order illustration"></img><br/>
             <li>
               Implementation steps:
               <ul class="font-xsm">
@@ -53,6 +78,68 @@ export default function DepthFirstSearch() {
                 <pre>
                   <code>
                     {preorder}
+                  </code>
+                </pre>
+              </div>
+            </li>
+          </ul>
+          <li>2) <u>Post-order</u></li>
+          <ul class="font-sm">
+            <li>Traverses the left subtree and then the right subtree, and lastly the root</li>
+            <li>Useful for deleting trees since we need to visit children nodes first before the parent</li><br/>
+            <img src={require('../images/DFSpostorder.png')} style={{width: "30vw"}} alt="DFS post-order illustration"></img><br/>
+            <li>
+              Implementation steps:
+              <ul class="font-xsm">
+                <li>Create a variable to store the values of nodes visited</li>
+                <li>Store the root of the BST in a variable to store the current</li>
+                <li>
+                  Write a helper function which accepts a node
+                  <ul class="font-xxsm">
+                    <li>If the node has a left property, call the helper function with the left property on the node</li>
+                    <li>If the node has a right property, call the helper function with the right property on the node</li>
+                    <li>Push the value of the node to the variable that stores the values</li> 
+                  </ul>
+                <li>Invoke the helper function with the current variable</li>
+                <li>Return the array of values</li>
+                </li>
+              </ul><br/>
+              JavaScript Implementation:
+              <div class="Code">
+                <pre>
+                  <code>
+                    {postorder}
+                  </code>
+                </pre>
+              </div>
+            </li>
+          </ul><br/>
+          <li>3) <u>In-order</u></li>
+          <ul class="font-sm">
+            <li>Traverses the left subtree and then the root and lastly the right subtree</li>
+            <li>Useful for retrieving the sorted contents of a Binary Search Tree in ascending order</li><br/>
+            <img src={require('../images/DFSinorder.png')} style={{width: "30vw"}} alt="DFS in-order illustration"></img><br/>
+            <li>
+              Implementation steps:
+              <ul class="font-xsm">
+                <li>Create a variable to store the values of nodes visited</li>
+                <li>Store the root of the BST in a variable to store the current</li>
+                <li>
+                  Write a helper function which accepts a node
+                  <ul class="font-xxsm">
+                    <li>If the node has a left property, call the helper function with the left property on the node</li>
+                    <li>Push the value of the node to the variable that stores the values</li> 
+                    <li>If the node has a right property, call the helper function with the right property on the node</li>                           
+                  </ul>
+                <li>Invoke the helper function with the current variable</li>
+                <li>Return the array of values</li>
+                </li>
+              </ul><br/>
+              JavaScript Implementation:
+              <div class="Code">
+                <pre>
+                  <code>
+                    {inorder}
                   </code>
                 </pre>
               </div>
