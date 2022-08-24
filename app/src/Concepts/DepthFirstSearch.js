443e67df31d40bf0961ee5859343ca7570689ccf
miltonhuynh <miltonhuynh@gmail.com>
2022-08-23 19:02:07 -0700

    Modifying DFS, switching devices

--- app/src/Concepts/DepthFirstSearch.js
+++ app/src/Concepts/DepthFirstSearch.js
@@ -4,6 +4,12 @@ export default function DepthFirstSearch() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Depth First Search</div>
+      <div class="Container font-m">
+        <ul>
+          <li>Starts at the root node and explores as far as possible along each branch before backtracking</li><br/>
+          <li>Time Complexity is the same as BFS, but takes up less space in trees with less depth</li>
+        </ul>
+      </div>
     </div>
   )
 }
