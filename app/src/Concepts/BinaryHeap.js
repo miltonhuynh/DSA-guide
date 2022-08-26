062df1775305068e8a7fee045c21c36b781bce58
miltonhuynh <miltonhuynh@gmail.com>
2022-08-26 14:09:07 -0700

    Added Binary Heap insert implementation steos

--- app/src/Concepts/BinaryHeap.js
+++ app/src/Concepts/BinaryHeap.js
@@ -25,13 +25,32 @@ export default function BinaryHeap() {
             <li>Also used with graph traversal algorithms</li>
             <li>
               Binary Heaps can be stored in a list or an array: 
-              <img src={require('../images/BinaryHeapArray.png')} style={{width: "5 0vw"}} alt="Binary Heap array illustration"></img><br/>
+              <img src={require('../images/BinaryHeapArray.png')} style={{width: "50vw"}} alt="Binary Heap array illustration"></img><br/>
               <ul class="font-sm">
                 <li>For any index of array n, the left child is stored at 2n + 1 and the right child is stored at 2n + 2</li>
                 <li>For any child at index n, its parent is at index (n - 1) / 2, floored</li>
               </ul>
             </li>
-            
+            <li>
+              Max Binary Heaps implementation: 
+              <ul class="font-sm">
+                <li>
+                  Insert:
+                  <ul class="font-xsm">
+                    <li>Push the value into the values property on the heap</li>
+                    <ul class="font-xxsm">
+                      <li>Create a variable called index which is the length of the values property - 1</li>
+                      <li>Create a variable called parentIndex which is the floor of (index - 1) / 2</li>
+                      <li>Keep looping as long as the values element at the parentIndex is less than the values element at the child index</li>
+                      <ul>
+                        <li>Swap the value of the values elementa at the parentIndex with the value of the element property at the child index</li>
+                        <li>Set the index to be the parentIndex, and start over</li>
+                      </ul>
+                    </ul>
+                  </ul>
+                </li>
+              </ul>
+            </li>
         </ul>
       </div>
     </div>
