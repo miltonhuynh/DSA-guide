3048c8aec53c64de8966476fca049b297c4efd5a
miltonhuynh <miltonhuynh@gmail.com>
2022-09-12 17:57:30 -0700

    Added uses for graphs and definitions for Graph page

--- app/src/Concepts/Graphs.js
+++ app/src/Concepts/Graphs.js
@@ -6,7 +6,26 @@ export default function Graphs() {
 			<div class="Concept_title">Graphs</div>
 			<div class="Container font-m">
 				<ul>
-					<li>A graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph</li>
+					<li>A graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph</li><br/>
+					<li>
+						Uses for graphs:
+						<ul class="font-sm">
+							<li>Social Networks</li>
+							<li>Location / Mapping</li>
+							<li>Routing Algorithms</li>
+							<li>Visual Hierachy</li>
+							<li>File System Optimizations</li>
+						</ul>
+					</li><br/>
+					<li>
+						Some definitions:
+						<ul class="font-sm">
+							<li><u>Vertex</u>: a node</li>
+							<li><u>Edge</u>: connection between nodes</li>
+							<li><u>Weighted/Unweighted</u>: values assigned to distances between vertices</li>
+							<li><u>Directed/Undirected</u>: directions assigned to distances between vertices</li>
+						</ul>
+					</li>
 				</ul>
 			</div>
 		</div>
