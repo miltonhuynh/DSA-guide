5e8cb3a88891055d3314ba926744e18579edb24e
miltonhuynh <miltonhuynh@gmail.com>
2022-09-16 16:27:00 -0700

    Added implementation code for Graphs

--- app/src/Concepts/Graphs.js
+++ app/src/Concepts/Graphs.js
@@ -1,6 +1,35 @@
 import React from 'react'
 
 export default function Graphs() {
+  const implementation =
+  `class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
+}`
+
   return (
 		<div class="Concept_page">
 			<div class="Concept_title">Graphs</div>
@@ -51,10 +80,22 @@ export default function Graphs() {
                   Big O<br/><br/>
                   <img src={require('../images/adj_compare.png')} style={{width: "40vw"}} alt="Adjacency comparison illustration"></img>
                 </li>
+                <li>Adjacency lists take up less space in sparse graphs in comparison to matrices</li>
+                <li>Adjacency lists are also faster to iterate over all edges</li>
+                <li>Adjacency matrices are faster to lookup a specific edge though</li>
+                <li>Adjacency lists tend to be better for the real world since in most situations, there are more vertices and less connections</li>
               </ul>
 						</ul>
 					</li>
 				</ul>
+        <div class="font-l">JavaScript Implementation</div>
+        <div class="Code">
+          <pre>
+            <code>
+              {implementation}
+            </code>
+          </pre>
+        </div>
 			</div>
 		</div>
 	)
