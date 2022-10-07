7f841c9cbfc5b59a9a29d069c32a6c578fffea9e
miltonhuynh <miltonhuynh@gmail.com>
2022-10-07 10:53:43 -0700

    Modified Djs algo page

--- app/src/Concepts/Dijkstra.js
+++ app/src/Concepts/Dijkstra.js
@@ -11,7 +11,16 @@ export default function Dijkstra() {
       <div class="Container font-m">
         <ul>
           <li>Dijkstra's algorithm is used to traverse <ref class="Link" onClick={() => navigate("../Graphs")}><u>Graphs</u></ref> and uses a <ref class="Link" onClick={() => navigate("../PriorityQueue")}><u>Priority Queue</u></ref></li><br/>
-          <li>Finds the shortest path between two vertices on a graph</li>
+          <li>Finds the shortest path between two vertices on a graph</li><br/>
+          <li>
+            Uses:
+            <ul class="font-sm">
+              <li>GPS: finding the fastest route</li>
+              <li>Network routing: finds open shortest path for data</li>
+              <li>Biology: used to model the spread of viruses among humans</li>
+              <li>Airline tickets: finding cheapest route to your destination</li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
