3509e91fa8fd04567a52688ecfaa976251a94a63
miltonhuynh <miltonhuynh@gmail.com>
2022-10-11 19:39:58 -0700

    Edited space complexity page

--- app/src/Concepts/SpaceComplexity.js
+++ app/src/Concepts/SpaceComplexity.js
@@ -4,6 +4,9 @@ export default function SpaceComplexity() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Space Complexity</div>
+      <div class="Container font-xl">
+        <div>Coming soon</div>
+      </div>
     </div>
   )
 }
