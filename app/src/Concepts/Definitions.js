a52455f5b36b3341535d6e29041eb5d1d62a0041
miltonhuynh <miltonhuynh@gmail.com>
2022-10-13 19:54:13 -0700

    Added dynamic programming definition

--- app/src/Concepts/Definitions.js
+++ app/src/Concepts/Definitions.js
@@ -8,6 +8,7 @@ export default function Definitions() {
           <div class="Term"><u>Abstract Data type</u>: a type (or class) for objects whose behaviour is defined by a set of value and a set of operations.</div>
           <div class="Term"><u>Class</u>: a blueprint for creating objects with pre-defined properties and methods.</div>
           <div class="Term"><u>Data structure</u>: collections of values, the relationships among them, and the functions or operations that can be applied to the data.</div>
+          <div class="Term"><u>Dynamic Programming</u>: a method of solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions</div>
           <div class="Term"><u>Greedy Algorithm: an approach for solving a problem by selecting the best option available at the moment. It doesn't worry whether the current best result will bring the overall optimal result.</u></div>
           <div class="Term"><u>Hashmap:</u> a way to store some data and retrieve it based off of a particular key.</div>
           <div class="Term"><u>Linked List</u>: a data structure that contains a head, tail and length property. Linked lists consists of nodes, and each node has a value and a pointer to another node or null.</div>
