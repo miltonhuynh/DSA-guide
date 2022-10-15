6c5788baf5e7d2e65e4c2eb8388a61e4be241cb0
miltonhuynh <miltonhuynh@gmail.com>
2022-10-15 13:34:57 -0700

    Modified DyProg page

--- app/src/Concepts/DynamicProgramming.js
+++ app/src/Concepts/DynamicProgramming.js
@@ -10,15 +10,27 @@ export default function DynamicProgramming() {
           <li>Dynamic programming is a method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions</li><br/>
           <li>Wherever we see a recursive solution that has repeated calls for same inputs, we can optimize it using Dynamic Programming. The idea is to simply store the results of subproblems, so that we do not have to re-compute them when needed later. This simple optimization reduces time complexities from exponential to polynomial.</li><br/>
           <li>
-            <u>Overlapping subproblems</u>
-            <ul class="font-sm">
-              <li>A problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times</li>
-              <li>
-                An example is the Fibonacci sequence, "every number after the first two is the sum of the two preceding ones", we are able to break down the problem into smaller steps<br/>
-                <img src={require('../images/fibnum.png')} style={{width: "40vw"}} alt="Fibonacci illustration"></img>
-              </li>
-
-            </ul>
+            There are two main properties that suggests that a problem can be solved using Dynamic programming:<br/>
+            <div class="indent">
+              <u>1) Overlapping subproblems</u>
+              <ul class="font-sm">
+                <li>A problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times</li>
+                <li>
+                  An example is the Fibonacci sequence, "every number after the first two is the sum of the two preceding ones", we are able to break down the problem into smaller steps (subproblems)<br/>
+                  <img src={require('../images/fibnum.png')} style={{width: "40vw"}} alt="Fibonacci illustration"></img>
+                </li>
+              </ul>
+            </div>
+            <div class="indent">
+              <u>2) Optimal substructure</u>
+              <ul class="font-sm">
+                <li>A problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems</li>
+                <li>
+                  An example is the Shortest Path Problem, if we know that A -> B -> C -> D is the shortest path from A to D, then A -> B -> C is the shortest path from A to C and A -> B is the shortest path from A to B
+                  <img src={require('../images/shortestpath.png')} style={{width: "40vw"}} alt="Shortest path illustration"></img>
+                </li>
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
--- app/src/Concepts/HeapSort.js
+++ app/src/Concepts/HeapSort.js
@@ -4,6 +4,9 @@ export default function HeapSort() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">Heap sort</div>
+      <div class="Container">
+        <div class="font-xxl">Coming soon</div>
+      </div>
     </div>
   )
 }
