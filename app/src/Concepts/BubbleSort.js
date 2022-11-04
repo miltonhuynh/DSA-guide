f897d817804bd3d2dda0abf8b9f448f255c5f844
miltonhuynh <miltonhuynh@gmail.com>
2022-11-03 19:21:05 -0700

    Removed periods in bubble sort

--- app/src/Concepts/BubbleSort.js
+++ app/src/Concepts/BubbleSort.js
@@ -21,12 +21,12 @@ export default function BubbleSort() {
       <div class="Concept_title">Bubble Sort</div>
       <div class="Container font-m">
         <ul>
-          <li>Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order.</li><br/>
-          <li>This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high.</li><br/>
-          <li>With each iteration, the largest number "bubbles" to the top.</li><br/>
+          <li>Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order</li><br/>
+          <li>This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high</li><br/>
+          <li>With each iteration, the largest number "bubbles" to the top</li><br/>
           <li>Implementation steps:</li>
           <div class="indent font-sm">
-            <div>1) Start looping with a variable called i at the end of the array towards the beginning.</div>
+            <div>1) Start looping with a variable called i at the end of the array towards the beginning</div>
             <div>2) Start an inner loop with a variable called j from the beginning until i - 1</div>
             <div>3) If arr[j] is greater than arr[j+1], swap those two values!</div>
             <div>4) Return the sorted array.</div><br/>
