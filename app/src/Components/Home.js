eecf789435b267bb3079da33cfa00347458dcd19
miltonhuynh <miltonhuynh@gmail.com>
2022-11-04 22:34:57 -0700

    Fixed typo on home page

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div class="Concept_title">Welcome!</div>
       <div class="Container font-m">
         <div>Data structures and algorithms can be challenging and I created this website to help myself and others break these concepts down and understand them better.</div><br/>
-        <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thourough explanations!</div><br/>
+        <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thorough explanations!</div><br/>
       </div>
     </div>
   )
