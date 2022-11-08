7e86605d25b928b4d8d7bc81c8e8eb854fe6428e
miltonhuynh <miltonhuynh@gmail.com>
2022-11-07 17:31:43 -0800

    Added home page bullet

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div class="Concept_page">
       <div class="Concept_title">Welcome!</div>
       <div class="Container font-m">
-        <div>Data structures and algorithms can be challenging and I created this website to help myself and others break these concepts down and understand them better.</div><br/>
-        <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thorough explanations!</div><br/>
+        <div>Data structures and algorithms can be challenging and I created this website to help myself and others break these concepts down and understand them better</div><br/>
+        <div>This site is designed to be a review and crash course rather than a beginner's guide, please visit other resources for more thorough explanations, I've added example LeetCode problems to the bottom of each page, these are designed to be difficult, don't be discouraged if you can't solve them!</div><br/>
       </div>
     </div>
   )
