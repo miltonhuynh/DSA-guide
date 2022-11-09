5833782ac158738ae744a38f9ca8ba11e58d5d32
miltonhuynh <miltonhuynh@gmail.com>
2022-11-08 20:06:14 -0800

    Commit just in case anything was missed

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div class="Concept_page">
       <div class="Concept_title">Welcome!</div>
       <div class="Container font-m">
-        <div>This website is meant to be used as refresher and crash course, please use other resources to learn new concepts for the first time</div><br/>
-        <div>I've added example LeetCode problems to the bottom of each page, these are designed to be difficult, don't be discouraged if you can't solve them!</div><br/>
+        <div>This website is meant to be used as refresher and crash course, use other resources to learn new concepts for the first time</div><br/>
+        <div>Leetcode problems at the bottom are designed to be difficult, even the ones marked easy, don't be discouraged</div><br/>
       </div>
     </div>
   )
