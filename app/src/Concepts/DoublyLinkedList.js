a833710f12a22ca5bc07946be6fca66246ebdbb5
Milton Huynh <miltonhuynh@gmail.com>
2022-11-08 17:02:02 -0800

    Fix, imported usenav to DLL page

--- app/src/Concepts/DoublyLinkedList.js
+++ app/src/Concepts/DoublyLinkedList.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function DoublyLinkedList() {
+
+  let navigate = useNavigate();
 
   const Implementation = 
   `class Node{
