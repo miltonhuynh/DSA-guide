bc8cf0929dc326f5aa068729d00107f969764380
miltonhuynh <miltonhuynh@gmail.com>
2022-11-09 17:07:33 -0800

    Added LC problem to binary tree

--- app/src/Concepts/BinaryTree.js
+++ app/src/Concepts/BinaryTree.js
@@ -161,9 +161,11 @@ class BinarySearchTree {
 				</div>
 				<div class="font-l">LeetCode examples</div>
 				<div class="font-m indent">
+						<li><a class="Link-green" href="https://leetcode.com/problems/binary-tree-inorder-traversal/">Binary Tree Inorder Traversal [Easy]</a></li>
 						<li><a class="Link-green" href="https://leetcode.com/problems/maximum-depth-of-binary-tree/">Maximum Depth of Binary Tree [Easy]</a></li>
 						<li><a class="Link-green" href="https://leetcode.com/problems/root-equals-sum-of-children/">Root Equals Sum of Children [Easy]</a></li>
 						<li><a class="Link-yellow" href="https://leetcode.com/problems/unique-binary-search-trees/">Unique Binary Search Trees [Medium]</a></li>
+						<li><a class="Link-yellow" href="hhttps://leetcode.com/problems/sum-root-to-leaf-numbers/">Sum Root to Leaf Numbers [Medium]</a></li>
 						<li><a class="Link-red" href="https://leetcode.com/problems/binary-tree-maximum-path-sum/">Binary Tree Maximum Path Sum [Hard]</a></li>
         </div><br/>
       </div>
