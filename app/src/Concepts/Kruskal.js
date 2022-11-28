f1c8ac247bde3a8f2f2f0b3315a9812a5a19c2cb
miltonhuynh <miltonhuynh@gmail.com>
2022-11-28 14:58:13 -0800

    Switching devices

--- app/src/Concepts/Kruskal.js
+++ app/src/Concepts/Kruskal.js
@@ -6,6 +6,10 @@ export default function Kruskal() {
     <div class="Concept_page">
       <div class="Concept_title">Kruskal's Algorithm</div>
       <div class="Container font-m">
+      Prerequisites: Prim's algorithm
+        <ul>
+          <li></li>
+        </ul>
       </div>
     </div>
   )
--- app/src/Concepts/Prim.js
+++ app/src/Concepts/Prim.js
@@ -6,6 +6,15 @@ export default function Prim() {
     <div class="Concept_page">
       <div class="Concept_title">Prim's Algorithm</div>
       <div class="Container font-m">
+        <ul>
+          <li>Solves the Minimal Cost Spanning Tree problem, which takes as input a connected, undirected Graph where each edge has a distance or weight measured attached, the Minimal Cost Spanning Tree is the graph containing the vertices of the Graph along with a subset of its edges that: </li>
+          <ul class="font-sm">
+            <li>1) Has minimum total cost as measured by summing the values for all of the edges in the subset</li>
+            <li>2) Keeps the vertices connected</li>
+          </ul>
+          <br/>
+          <li>The MCST contains no cycles. If a proposed MCST did have a cycle, a cheaper MCST could be had by removing any one of the edges in the cycle. Thus, the MCST is a free tree with |V|−1 edges. The name “minimum-cost spanning tree” comes from the fact that the required set of edges forms a tree, it spans the vertices (i.e., it connects them together), and it has minimum cost</li>
+        </ul>
       </div>
     </div>
   )
