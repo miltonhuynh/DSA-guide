010a1a5161f7161feef9c1ba877e3fcce4699287
miltonhuynh <miltonhuynh@gmail.com>
2022-11-29 20:06:22 -0800

    Edited prim's algo

--- app/src/Concepts/Prim.js
+++ app/src/Concepts/Prim.js
@@ -13,7 +13,8 @@ export default function Prim() {
             <li>2) Keeps the vertices connected</li>
           </ul>
           <br/>
-          <li>The MCST contains no cycles. If a proposed MCST did have a cycle, a cheaper MCST could be had by removing any one of the edges in the cycle. Thus, the MCST is a free tree with |V|−1 edges. The name “minimum-cost spanning tree” comes from the fact that the required set of edges forms a tree, it spans the vertices (i.e., it connects them together), and it has minimum cost</li>
+          <li>The MCST contains no cycles. If a proposed MCST did have a cycle, a cheaper MCST could be had by removing any one of the edges in the cycle. Thus, the MCST is a free tree with |V|−1 edges. The name “minimum-cost spanning tree” comes from the fact that the required set of edges forms a tree, it spans the vertices (i.e., it connects them together), and it has minimum cost</li><br/>
+          <li>Prim's algorithm is a greedy algorithm which selects the best option at each step, it always starts with a single node and it moves through several adjacent nodes, in order to explore all of the connected edges along the way.</li>
         </ul>
       </div>
     </div>
