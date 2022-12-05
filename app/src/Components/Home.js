92434e9fbd553d62f2818b223d7146ce19bdc0b6
miltonhuynh <miltonhuynh@gmail.com>
2022-12-04 23:46:58 -0800

    reduced header size

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -5,8 +5,7 @@ export default function Home() {
     <div class="Concept_page">
       <div class="Concept_title">Welcome!</div>
       <div class="Container font-m">
-        <div>This website is meant to be used as refresher and crash course, use other resources to learn new concepts for the first time</div><br/>
-        <div>Leetcode problems at the bottom are designed to be difficult, even the ones marked easy, don't be discouraged</div><br/>
+        <div>This website is meant to be used as refresher before an interview, please use other resources to learn new concepts for the first time</div><br/>
       </div>
     </div>
   )
