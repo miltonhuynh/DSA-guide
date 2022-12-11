7f025dad74f413fba0fb2877ce6e9dab22d67806
miltonhuynh <miltonhuynh@gmail.com>
2022-12-10 16:19:06 -0800

    Formatting fixes

--- app/src/Concepts/InsertionSort.js
+++ app/src/Concepts/InsertionSort.js
@@ -5,16 +5,16 @@ export default function InsertionSort() {
   const insertion = 
   `function insertionSort(arr){
     var currentVal;
-      for(var i = 1; i < arr.length; i++){
-          currentVal = arr[i];
-          for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-              arr[j+1] = arr[j]
-          }
-          arr[j+1] = currentVal;
-      }
-      return arr;
-  }
-  `
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal;
+    }
+    return arr;
+  }`
+
   return (
     <div class="Concept_page">
       <div class="Concept_title">Insertion sort</div>
@@ -30,7 +30,7 @@ export default function InsertionSort() {
                 <div>1) Start by picking the second element in the array.</div>
                 <div>2) Compare the second element with the one before it and swap if necessary.</div>
                 <div>3) Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place.</div>
-                <div>4) Repeat until the array is sorted.</div><br/>
+                <div>4) Repeat until the array is sorted.</div>
               </div>
             </div>
             <div class="font-l">JavaScript Implementation</div>
