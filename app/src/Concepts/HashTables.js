fecbce38f1ac10ab870becec3b650d2177305819
miltonhuynh <miltonhuynh@gmail.com>
2022-12-20 22:11:14 -0800

    updated hash tables page to new style

--- app/src/Concepts/HashTables.js
+++ app/src/Concepts/HashTables.js
@@ -80,118 +80,129 @@ export default function HashTables() {
       <div class="Concept_title">Hash tables</div>
       <div class="Container font-m">
         <ul>
-          <li>Used to store key-value pairs</li><br/>
-          <li>Like arrays, but keys are not ordered</li><br/>
-          <li>
-            Fast for the following operations, unlike arrays:
-            <ul class="font-sm">
-              <li>Finding values</li>
-              <li>Adding new values</li>
-              <li>Removing values</li>
-            </ul><br/>
-          </li>
-          <li>Nearly every programming language has some sort of hash table data structure</li><br/>
-          <li>
-            Because of their speed, hash tables are very commonly used
-            <ul class="font-sm">
-              <li>Python has Dictionaries</li>
-              <li>JavaScript has Objects and Maps</li>
-              <li>Java, Go and Scala have Maps</li>
-              <li>Ruby has hashes</li>
-            </ul><br/>
-          </li>
-          <li>
-            Big O of Hash Tables
-            <ul class="font-sm"> 
-              <li>Insertion: O(1)</li>
-              <li>Deletion: O(1)</li>
-              <li>Access: O(1)</li>
-            </ul>
-          </li><br/>
-          <li>
-            An array can be used to implement a hash table: 
-            <ul class="font-sm">
-              <li>In order to look up values by key, we need a way to convert keys into valid array indices</li>
-              <li>A function that performs this task is called a hash function</li>
-              <li>Hash functions use prime numbers to reduce collisions and spread out keys more uniformly</li>
-              <li>
-                A good hash function has to be: 
-                <ul class="font-xsm">
-                  <li>Fast (constant time)</li>
-                  <li>Doesn't cluster outputs at specific indices, but distributes uniformly</li>
-                  <li>Deterministic (same input yields same output)</li>
-                </ul><br/>
+          <div class="Section"><li>Used to store key-value pairs</li></div>
+          <div class="Section"><li>Like arrays, but keys are not ordered</li></div>
+          <div class="Section">
+            <li>
+              Fast for the following operations, unlike arrays:
+              <ul class="font-sm">
+                <li>Finding values</li>
+                <li>Adding new values</li>
+                <li>Removing values</li>
+              </ul><br/>
+            </li>
+          </div>
+          <div class="Section"><li>Nearly every programming language has some sort of hash table data structure</li></div>
+          <div class="Section">
+            <li>
+              Because of their speed, hash tables are very commonly used
+              <ul class="font-sm">
+                <li>Python has Dictionaries</li>
+                <li>JavaScript has Objects and Maps</li>
+                <li>Java, Go and Scala have Maps</li>
+                <li>Ruby has hashes</li>
+              </ul>
+            </li>
+          </div>
+          <div class="Section">
+            <li>
+              Big O of Hash Tables
+              <ul class="font-sm"> 
+                <li>Insertion: O(1)</li>
+                <li>Deletion: O(1)</li>
+                <li>Access: O(1)</li>
+              </ul>
+            </li>
+          </div>
+          <div class="Section">
+            <li>
+              An array can be used to implement a hash table: 
+              <ul class="font-sm">
+                <li>In order to look up values by key, we need a way to convert keys into valid array indices</li>
+                <li>A function that performs this task is called a hash function</li>
+                <li>Hash functions use prime numbers to reduce collisions and spread out keys more uniformly</li>
                 <li>
-                  Example of a hash function that takes in a string and returns a key:
-                  <div class="Code-sm">
-                    <pre>
-                      <code>
-                        {hashfunction}
-                      </code>
-                    </pre>
-                  </div>
-                </li>
-                <li>Handling collisions: </li>
-                <ul class="font-xsm">
-                  <li>Even with a large array and good hash function, collisions are inevitable</li>
+                  A good hash function has to be: 
+                  <ul class="font-xsm">
+                    <li>Fast (constant time)</li>
+                    <li>Doesn't cluster outputs at specific indices, but distributes uniformly</li>
+                    <li>Deterministic (same input yields same output)</li>
+                  </ul><br/>
+                
                   <li>
-                    The two main strategies to deal with collisions are:
-                    <ul class="font-xxsm">
-                      <li>1) <u>Separate chaining</u>: store values using a more sophisticated data structure at each index in the array, this allows us to store multiple key-value pairs at the same index</li>
-                      <li>2) <u>Linear probing</u>: when a collision is found, search through the array to find the next empty spot</li>
-                    </ul><br/>
+                    Example of a hash function that takes in a string and returns a key:
+                    <div class="Code-sm">
+                      <pre>
+                        <code>
+                          {hashfunction}
+                        </code>
+                      </pre>
+                    </div>
                   </li>
-                </ul>
-                <li>Implementation Steps:</li>
-                <ul class="font-xsm">
-                  <li>Set</li>
-                  <ul class="font-xxsm">
-                    <li>Accepts a key and a value</li>
-                    <li>Hashes the key</li>
-                    <li>Store the key-value pair in the hash table array via separate chaining</li>
-                    <li>If the key isn't found, return underfined</li>
+                  <li>Handling collisions: </li>
+                  <ul class="font-xsm">
+                    <li>Even with a large array and good hash function, collisions are inevitable</li>
+                    <li>
+                      The two main strategies to deal with collisions are:
+                      <ul class="font-xxsm">
+                        <li>1) <u>Separate chaining</u>: store values using a more sophisticated data structure at each index in the array, this allows us to store multiple key-value pairs at the same index</li>
+                        <li>2) <u>Linear probing</u>: when a collision is found, search through the array to find the next empty spot</li>
+                      </ul><br/>
+                    </li>
                   </ul>
-                </ul>
-                <ul class="font-xsm">
-                  <li>Get</li>
-                  <ul class="font-xxsm">
-                    <li>Accepts a key</li>
-                    <li>Hashes the key</li>
-                    <li>Retrieves the key-value pair in the hash table</li>
+                  <li>Implementation Steps:</li>
+                  <ul class="font-xsm">
+                    <li>Set</li>
+                    <ul class="font-xxsm">
+                      <li>Accepts a key and a value</li>
+                      <li>Hashes the key</li>
+                      <li>Store the key-value pair in the hash table array via separate chaining</li>
+                      <li>If the key isn't found, return underfined</li>
+                    </ul>
                   </ul>
-                </ul>
-                <ul class="font-xsm">
-                  <li>Keys</li>
-                  <ul class="font-xxsm">
-                    <li>Loops through the hash table array and returns an array of keys in the table</li>
+                  <ul class="font-xsm">
+                    <li>Get</li>
+                    <ul class="font-xxsm">
+                      <li>Accepts a key</li>
+                      <li>Hashes the key</li>
+                      <li>Retrieves the key-value pair in the hash table</li>
+                    </ul>
                   </ul>
-                </ul>
-                <ul class="font-xsm">
-                  <li>Values</li>
-                  <ul class="font-xxsm">
-                    <li>Loops through the hash table array and returns an array of values in the table</li>
+                  <ul class="font-xsm">
+                    <li>Keys</li>
+                    <ul class="font-xxsm">
+                      <li>Loops through the hash table array and returns an array of keys in the table</li>
+                    </ul>
                   </ul>
-                </ul>
-              </li>
-            </ul>
-          </li>
+                  <ul class="font-xsm">
+                    <li>Values</li>
+                    <ul class="font-xxsm">
+                      <li>Loops through the hash table array and returns an array of values in the table</li>
+                    </ul>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </div>
         </ul>
         <div class="font-l">JavaScript Implementation</div>
-        <div class="Code">
+        <div class="Code Section">
           <pre>
             <code>
               {implementation}
             </code>
           </pre>
         </div>
-        <div class="font-l">
-          LeetCode examples
-          <div class="font-m indent">
-              <li><a class="Link-green" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/two-sum/">Two Sum [Easy]</a></li>
-              <li><a class="Link-yellow" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/reduce-array-size-to-the-half/">Reduce Array Size to The Half [Medium]</a></li>
-              <li><a class="Link-red" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/word-ladder/">Word Ladder [Hard]</a></li>
+        <div class="Section">
+          <div class="font-l">
+            LeetCode examples
+            <div class="font-m indent">
+                <li><a class="Link-green" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/two-sum/">Two Sum [Easy]</a></li>
+                <li><a class="Link-yellow" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/reduce-array-size-to-the-half/">Reduce Array Size to The Half [Medium]</a></li>
+                <li><a class="Link-red" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/word-ladder/">Word Ladder [Hard]</a></li>
+            </div>
           </div>
-        </div><br/>
+        </div>
       </div>
     </div>
   )
