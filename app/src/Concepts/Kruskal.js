a17f9238749877b35f092010fc47922a9c072789
miltonhuynh <miltonhuynh@gmail.com>
2022-12-21 19:25:37 -0800

    Updated Kruskal's alrogithm to new style

--- app/src/Concepts/Kruskal.js
+++ app/src/Concepts/Kruskal.js
@@ -45,37 +45,42 @@ export default function Kruskal() {
           <ref class="Link" onClick={() => navigate("../Prim")}> <u>Prim's Algorithm</u></ref>
         </li>
         <ul>
-          <li>Solves the Minimal Cost Spanning Tree problem, which takes as input a connected, undirected Graph where each edge has a distance or weight measured attached, the Minimal Cost Spanning Tree is the graph containing the vertices of the Graph along with a subset of its edges that: </li>
-          <ul class="font-sm">
-            <li>1) Has minimum total cost as measured by summing the values for all of the edges in the subset</li>
-            <li>2) Keeps the vertices connected</li>
-            <li>3) Contains no cycles</li>
-          </ul>
-          <br/>
-          <li>Prim's algorithm is a greedy algorithm which selects the best option at each step</li><br/>
-          <li>
-            How Kruskal's algorithm works: 
+          <div class="Section">
+            <li>Solves the Minimal Cost Spanning Tree problem, which takes as input a connected, undirected Graph where each edge has a distance or weight measured attached, the Minimal Cost Spanning Tree is the graph containing the vertices of the Graph along with a subset of its edges that: </li>
             <ul class="font-sm">
-              <li>1) Sort all the edges from low weight to high</li>
-              <li>2) Take the edge with the lowest weight and add it to the spanning tree. If adding the edge created a cycle, then reject this edge</li>
-              <li>3) Keep adding edges until we reach all vertices</li>
+              <li>1) Has minimum total cost as measured by summing the values for all of the edges in the subset</li>
+              <li>2) Keeps the vertices connected</li>
+              <li>3) Contains no cycles</li>
             </ul>
-          </li><br/>
-          <li>The time complexity Of Kruskal's Algorithm is: O(E log E).</li>
+          </div>
+          <div class="Section"><li>Prim's algorithm is a greedy algorithm which selects the best option at each step</li></div>
+          <div class="Section">
+            <li>
+              How Kruskal's algorithm works: 
+              <ul class="font-sm">
+                <li>1) Sort all the edges from low weight to high</li>
+                <li>2) Take the edge with the lowest weight and add it to the spanning tree. If adding the edge created a cycle, then reject this edge</li>
+                <li>3) Keep adding edges until we reach all vertices</li>
+              </ul>
+            </li>
+          </div>
+          <div class="Section"><li>The time complexity Of Kruskal's Algorithm is: O(E log E).</li></div>
         </ul>
         <div class="font-l">JavaScript Implementation</div>
-        <div class="Code">
+        <div class="Code Section">
           <pre>
             <code>
               {Implementation}
             </code>
           </pre>
         </div>
-        <div class="font-l">LeetCode examples</div>
-          <div class="font-m indent">
-            <li><a class="Link-yellow" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/min-cost-to-connect-all-points/">Min Cost to Connect All Points [Medium]</a></li>
-            <li><a class="Link-red" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/">Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree [Hard]</a></li>
-        </div><br/>
+        <div class="Section">
+          <div class="font-l">LeetCode examples</div>
+            <div class="font-m indent">
+              <li><a class="Link-yellow" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/min-cost-to-connect-all-points/">Min Cost to Connect All Points [Medium]</a></li>
+              <li><a class="Link-red" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/">Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree [Hard]</a></li>
+          </div>
+        </div>
       </div>
     </div>
   )
