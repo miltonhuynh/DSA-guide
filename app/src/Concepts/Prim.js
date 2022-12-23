3c05e15df3970bc809b67b2cb47cacedc4c8c502
miltonhuynh <miltonhuynh@gmail.com>
2022-12-22 18:08:27 -0800

    Formatted Prim algo page to new style

--- app/src/Concepts/Prim.js
+++ app/src/Concepts/Prim.js
@@ -62,36 +62,42 @@ export default function Prim() {
       <div class="Concept_title">Prim's Algorithm</div>
       <div class="Container font-m">
         <ul>
-          <li>Solves the Minimal Cost Spanning Tree problem, which takes as input a connected, undirected Graph where each edge has a distance or weight measured attached, the Minimal Cost Spanning Tree is the graph containing the vertices of the Graph along with a subset of its edges that: </li>
-          <ul class="font-sm">
-            <li>1) Has minimum total cost as measured by summing the values for all of the edges in the subset</li>
-            <li>2) Keeps the vertices connected</li>
-            <li>3) Contains no cycles</li>
-          </ul>
-          <br/>
-          <li>Prim's algorithm is a greedy algorithm which selects the best option at each step, it always starts with a single node and it moves through several adjacent nodes, in order to explore all of the connected edges along the way.</li><br/>
-          <li>
-            We start from one vertex and keep adding edges with the lowest weight until we reach our goal.
+          <div class="Section">
+            <li>Solves the Minimal Cost Spanning Tree problem, which takes as input a connected, undirected Graph where each edge has a distance or weight measured attached, the Minimal Cost Spanning Tree is the graph containing the vertices of the Graph along with a subset of its edges that: </li>
             <ul class="font-sm">
-              <li>1) Initialize the minimum spanning tree with a vertex chosen at random.</li>
-              <li>2) Find all the edges that connect the tree to new vertices, find the minimum and add it to the tree</li>
-              <li>3) Keep repeating step 2 until we get a minimum spanning tree, if there are multiple choices, choose one at random</li>
+              <li>1) Has minimum total cost as measured by summing the values for all of the edges in the subset</li>
+              <li>2) Keeps the vertices connected</li>
+              <li>3) Contains no cycles</li>
             </ul>
-          </li><br/>
+          </div>
+          <br/>
+          <div class="Section"><li>Prim's algorithm is a greedy algorithm which selects the best option at each step, it always starts with a single node and it moves through several adjacent nodes, in order to explore all of the connected edges along the way.</li></div>
+          <div class="Section">
+            <li>
+              We start from one vertex and keep adding edges with the lowest weight until we reach our goal.
+              <ul class="font-sm">
+                <li>1) Initialize the minimum spanning tree with a vertex chosen at random.</li>
+                <li>2) Find all the edges that connect the tree to new vertices, find the minimum and add it to the tree</li>
+                <li>3) Keep repeating step 2 until we get a minimum spanning tree, if there are multiple choices, choose one at random</li>
+              </ul>
+            </li>
+          </div>
         </ul>
         <div class="font-l">JavaScript Implementation</div>
-        <div class="Code">
+        <div class="Code Section">
           <pre>
             <code>
               {Implementation}
             </code>
           </pre>
         </div>
-        <div class="font-l">LeetCode examples</div>
-          <div class="font-m indent">
-            <li><a class="Link-yellow" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/min-cost-to-connect-all-points/">Min Cost to Connect All Points [Medium]</a></li>
-            <li><a class="Link-red" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/">Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree [Hard]</a></li>
-        </div><br/>
+        <div class="Section">
+          <div class="font-l">LeetCode examples</div>
+            <div class="font-m indent">
+              <li><a class="Link-yellow" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/min-cost-to-connect-all-points/">Min Cost to Connect All Points [Medium]</a></li>
+              <li><a class="Link-red" rel="noreferrer" target="_blank" href="https://leetcode.com/problems/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/">Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree [Hard]</a></li>
+          </div>
+        </div>
       </div>
     </div>
   )
