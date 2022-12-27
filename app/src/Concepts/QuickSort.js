c92b26b7f277998ffc8c2ffd7c7ab94cb79d503f
miltonhuynh <miltonhuynh@gmail.com>
2022-12-27 14:40:13 -0800

    Updated quick sort page to new style

--- app/src/Concepts/QuickSort.js
+++ app/src/Concepts/QuickSort.js
@@ -33,25 +33,30 @@ export default function QuickSort() {
        return arr;
   }`
 
+  const n_squared = 
+  `\u00B2`
+
   return (
     <div class="Concept_page">
       <div class="Concept_title">Quick sort</div>
       <div class="Container">
         <ul>
           <div class="font-m">
-            <li>Works by selecting one element (the pivot) and finding the index where the pivot should end up in the sorted array.</li><br/>
-            <li>Counts the number of elements on each side of the pivot.</li><br/>
-            <li>Once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot.</li><br/>
-            <li>The time complexity of this sorting algorithm is on average O(n log(n)) and worst case n^2 and is in-place.</li><br/>
-            <li>Implementation Steps:</li>
-            <div class="indent font-sm">
-              <div>1) Choose an element in the array to be the pivot.</div>
-              <div>2) Iterate through the array and find the index where the pivot should be, swap so all numbers less than pivot is on the left side of the array and greater than to the right.</div>
-              <div>3) Swap the pivot element with the element in its correct index.</div>
-              <div>4) Recursively repeat process until the array is sorted.</div>
-            </div><br/>
+            <div class="Section"><li>Works by selecting one element (the pivot) and finding the index where the pivot should end up in the sorted array.</li></div>
+            <div class="Section"><li>Counts the number of elements on each side of the pivot.</li></div>
+            <div class="Section"><li>Once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot.</li></div>
+            <div class="Section"><li>The time complexity of this sorting algorithm is on average O(n log(n)) and worst case O(n{n_squared}) and is in-place.</li></div>
+            <div class="Section">
+              <li>Implementation Steps:</li>
+              <div class="indent font-sm">
+                <div>1) Choose an element in the array to be the pivot.</div>
+                <div>2) Iterate through the array and find the index where the pivot should be, swap so all numbers less than pivot is on the left side of the array and greater than to the right.</div>
+                <div>3) Swap the pivot element with the element in its correct index.</div>
+                <div>4) Recursively repeat process until the array is sorted.</div>
+              </div>
+            </div>
             <div class="font-l">JavaScript Implementation</div>
-            <div class="Code">
+            <div class="Code Section">
               <pre>
                 <code>
                   {Implementation}
