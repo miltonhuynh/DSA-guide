c32ad066e2ce61c46779465d83be8f81f4bf9033
miltonhuynh <miltonhuynh@gmail.com>
2022-12-31 11:45:56 -0800

    Updated space complexity page to new style

--- app/src/Concepts/SpaceComplexity.js
+++ app/src/Concepts/SpaceComplexity.js
@@ -5,9 +5,11 @@ export default function SpaceComplexity() {
     <div class="Concept_page">
       <div class="Concept_title">Space Complexity</div>
       <div class="Container font-m">
-        <div>Space complexity refers to the total amount of memory space used by an algorithm/program, including the space of input values for execution. </div><br/>
-        <div>When an algorithm is run on a computer, it necessitates a certain amount of memory space. The amount of memory used by a program to execute it is represented by its space complexity. Because a program requires memory to store input data and temporal values while running, the space complexity is auxiliary and input space.</div><br/>
-        <div>Space complexity depends on a variety of things such as the programming language, the compiler, or even the machine running the algorithm</div><br/>
+        <ul>
+          <div class="Section"><li>Space complexity refers to the total amount of memory space used by an algorithm/program, including the space of input values for execution. </li></div>
+          <div class="Section"><li>When an algorithm is run on a computer, it necessitates a certain amount of memory space. The amount of memory used by a program to execute it is represented by its space complexity. Because a program requires memory to store input data and temporal values while running, the space complexity is auxiliary and input space.</li></div>
+          <div class="Section"><li>Space complexity depends on a variety of things such as the programming language, the compiler, or even the machine running the algorithm</li></div>
+        </ul>
       </div>
     </div>
   )
