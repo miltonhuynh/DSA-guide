c9bf546a5ef71257ed400faeeb57e35df226d23d
miltonhuynh <miltonhuynh@gmail.com>
2023-01-04 14:58:02 -0800

    Updated String Search page to new style

--- app/src/Concepts/StringSearch.js
+++ app/src/Concepts/StringSearch.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function StringSearch() {
-    const implementation =
+  const implementation =
     `function counter(sequence, whole_string) {
         let count = 0;
       
@@ -23,24 +23,29 @@ export default function StringSearch() {
   return (
     <div class="Concept_page">
       <div class="Concept_title">String Search</div>
-      <div class="Container">
+      <div class="Container font-m">
         <ul>
-            <li><div class="font-m">This is a method to count a number of times a smaller string appears in a larger string.</div></li><br/>
-            <li><div class="font-m">Implementation steps:</div></li>
-            <div class="font-sm indent">1. Loop over the longer string.</div>
-            <div class="font-sm indent">2. Loop over the shorter string.</div>
-            <div class="font-sm indent">3. If the characters don't match, break out of the inner loop.</div>
-            <div class="font-sm indent">4. If the characters do match, keep going.</div>
-            <div class="font-sm indent">5. If you complete the inner loop and find a match, increment the count of matches.</div>
-            <div class="font-sm indent">6. Return the count.</div><br/>
-            <div class="font-l">JavaScript Implementation: </div>
-            <div class="Code">
-                <pre>
-                    <code>
-                        {implementation}
-                    </code>
-                </pre>
-            </div>
+          <div class="Section"><li>This is a method to count a number of times a smaller string appears in a larger string.</li></div>
+          <div class="Section">
+            <li>Implementation steps:
+              <ul>
+                <li>1. Loop over the longer string.</li>
+                <li>2. Loop over the shorter string.</li>
+                <li>3. If the characters don't match, break out of the inner loop.</li>
+                <li>4. If the characters do match, keep going.</li>
+                <li>5. If you complete the inner loop and find a match, increment the count of matches.</li>
+                <li>6. Return the count.</li>
+              </ul>
+            </li>
+          </div>
+          <div class="font-l">JavaScript Implementation: </div>
+          <div class="Code Section">
+            <pre>
+              <code>
+                {implementation}
+              </code>
+            </pre>
+          </div>
         </ul>
       </div>
     </div>
