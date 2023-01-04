be5f4de579b2a48ce71c563c18896b016add0f32
miltonhuynh <miltonhuynh@gmail.com>
2023-01-03 18:11:28 -0800

    Updated stack page to new style

--- app/src/Concepts/Stack.js
+++ app/src/Concepts/Stack.js
@@ -52,54 +52,66 @@ class Stack {
       <div class="Container">
         <ul>
           <div class="font-m">
-            <li>A last in first out data structure, the last element added to the stack will be the first element removed from the stack.</li><br/>
-            <li>Stacks are used to handle function invocations (the call stack), for operations like undo/redo, and for routing and much more</li><br/>
-            <li>Not a built in structure in JavaScript</li><br/>
-            <li>
-              Big O of Stacks
-              <div class="indent-extra font-sm">
-                <div>Insertion: O(1)</div>
-                <div>Removal: O(1)</div>
-                <div>Searching: O(n)</div>
-                <div>Access: O(n)</div>
-              </div>
-            </li><br/>
-            <li>
-              There are multiple ways to implement a stack in JavaScript, one way is to create an array and use the push and pop methods<br/><br/>
-              Example:
-              <div class="Code">
-                <pre>
-                  <code>
-                    {array_example}
-                  </code>
-                </pre>
-              </div>
-              <div class="font-sm">Since the item at index 1 '2' was added last to the list it will be removed when pop is called</div><br/>
-            </li>
-            <li>In addition to push and pop, unshift and shift can also be used to implement a stack in an array, these add and remove from the beginning of the array instead of the end; however, it is better to push and pops to avoid re-indexing every other item</li><br/>
-            <li>Linked lists can also be used to implement a Stack data structure</li><br/>
-            <li>Implementation steps</li>
-            <ul><li>Push (Adds a node to the top of the stack)</li></ul>
-            <div class="indent-extra font-sm">
-              <div>1) The function should accept a value</div>
-              <div>2) Create a new node with that value</div>
-              <div>3) If there are no nodes in the stack, set the first and last property to be the newly created node</div>
-              <div>4) If there is at least one node, create a variable that stores the current first property on the Stack</div>
-              <div>5) Reset the first property to be the newly created node</div>
-              <div>6) Set the next property on the node to be the previously created variable</div>
-              <div>7) Increment the size of the stack by 1</div>
+            <div class="Section"><li>A last in first out data structure, the last element added to the stack will be the first element removed from the stack.</li></div>
+            <div class="Section"><li>Stacks are used to handle function invocations (the call stack), for operations like undo/redo, and for routing and much more</li></div>
+            <div class="Section"><li>Not a built in structure in JavaScript</li></div>
+            <div class="Section">
+              <li>
+                Big O of Stacks
+                <ul>
+                  <li>Insertion: O(1)</li>
+                  <li>Removal: O(1)</li>
+                  <li>Searching: O(n)</li>
+                  <li>Access: O(n)</li>
+                </ul>
+              </li>
             </div>
-            <ul><li>Pop (Removes a node from the top of the stack)</li></ul>
-            <div class="indent-extra font-sm">
-              <div>8) If there are no nodes in the stack, return null</div>
-              <div>9) Create a temporary variable to store the first property on the stack</div>
-              <div>10) If there is only one node, set the first and last property to be null</div>
-              <div>11) If there is more than one node, set the first property to be the next property on the current list</div>
-              <div>12) Decrement the size by one</div>
-              <div>13) Return the value of the node removed</div><br/>
+            <div class="Section">
+              <li>
+                There are multiple ways to implement a stack in JavaScript, one way is to create an array and use the push and pop methods<br/><br/>
+                Example:
+                <div class="Code">
+                  <pre>
+                    <code>
+                      {array_example}
+                    </code>
+                  </pre>
+                </div>
+                <div class="font-sm">Since the item at index 1 '2' was added last to the list it will be removed when pop is called.</div>
+              </li>
+            </div>
+            <div class="Section"><li>In addition to push and pop, unshift and shift can also be used to implement a stack in an array, these add and remove from the beginning of the array instead of the end; however, it is better to push and pops to avoid re-indexing every other item</li></div>
+            <div class="Section"><li>Linked lists can also be used to implement a Stack data structure</li></div>
+            <div class="Section">
+              <li>Implementation steps</li>
+              <ul class="font-sm">
+                <li>Push (Adds a node to the top of the stack)
+                  <ul class="font-xsm">
+                    <li>1) The function should accept a value</li>
+                    <li>2) Create a new node with that value</li>
+                    <li>3) If there are no nodes in the stack, set the first and last property to be the newly created node</li>
+                    <li>4) If there is at least one node, create a variable that stores the current first property on the Stack</li>
+                    <li>5) Reset the first property to be the newly created node</li>
+                    <li>6) Set the next property on the node to be the previously created variable</li>
+                    <li>7) Increment the size of the stack by 1</li>
+                  </ul>
+                </li>
+              </ul>
+              <ul class="font-sm">
+                <li>Pop (Removes a node from the top of the stack)
+                  <ul>
+                    <li>8) If there are no nodes in the stack, return null</li>
+                    <li>9) Create a temporary variable to store the first property on the stack</li>
+                    <li>10) If there is only one node, set the first and last property to be null</li>
+                    <li>11) If there is more than one node, set the first property to be the next property on the current list</li>
+                    <li>12) Decrement the size by one</li>
+                    <li>13) Return the value of the node removed</li>
+                  </ul>
+                </li>
+              </ul>
             </div>
             <div class="font-l">Implementation</div>
-            <div class="Code">
+            <div class="Code Section">
               <pre>
                 <code>
                   {DLL_example}
