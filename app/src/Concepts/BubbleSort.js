8a9b9a9c7dd5a4cfa6d82878c7a120dd81dc063f
miltonhuynh <miltonhuynh@gmail.com>
2023-02-01 20:04:18 -0800

    semantically improved bubble sort page

--- app/src/Concepts/BubbleSort.js
+++ app/src/Concepts/BubbleSort.js
@@ -21,13 +21,13 @@ export default function BubbleSort() {
 
   return (
     <div class="Concept_page">
-      <div class="Concept_title">Bubble Sort</div>
+      <h1>Bubble Sort</h1>
       <div class="Container font-m">
         <ul>
-          <div class="Section"><li>Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order</li></div>
-          <div class="Section"><li>This algorithm is not suitable for large data sets as its average and worst case time complexity is O(n{n_squared})</li></div>
-          <div class="Section"><li>With each iteration, the largest number "bubbles" to the top</li></div>
-          <div class="Section">
+          <section><li>Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order</li></section>
+          <section><li>This algorithm is not suitable for large data sets as its average and worst case time complexity is O(n{n_squared})</li></section>
+          <section><li>With each iteration, the largest number "bubbles" to the top</li></section>
+          <section>
             <li>Implementation steps:</li>
             <div class="indent font-sm">
               <div>1) Start looping with a variable called i at the end of the array towards the beginning</div>
@@ -35,7 +35,7 @@ export default function BubbleSort() {
               <div>3) If arr[j] is greater than arr[j+1], swap those two values!</div>
               <div>4) Return the sorted array.</div>
             </div>
-          </div>
+          </section>
         </ul>
         <div class="font-l">Javascript Implementation</div>
           <div class="Code Section">
