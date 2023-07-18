714adf696764fba2bc7fa32e99ecc739bf8829ff
miltonhuynh <miltonhuynh@gmail.com>
2023-07-18 15:16:20 -0700

    added home landing page for user login

--- app/src/Components/Landing.js
+++ app/src/Components/Landing.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div class="Concept_page">
       <div class="Concept_title">Welcome!</div>
       <div class="Container">
-        <div class="Section font-sm">Log in or continue as guest placeholder</div>
+        <div class="Section font-sm">I created this website to be a quick reference while doing coding challenges, please use other resources to learn concepts for the first time!</div>
       </div>
     </div>
   )
