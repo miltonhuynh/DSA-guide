f35490d351eeeff419401054dea72d71424e6154
miltonhuynh <miltonhuynh@gmail.com>
2023-07-21 14:00:25 -0700

    removed animate presence

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import HeaderFooter from './Components/HeaderFooter';
 import Page from './Components/Page';
-import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
-    <AnimatePresence>
-      <HeaderFooter>
-        <Page/>
-      </HeaderFooter>
-    </AnimatePresence>
+    <HeaderFooter>
+      <Page/>
+    </HeaderFooter>
   );
 }
 
