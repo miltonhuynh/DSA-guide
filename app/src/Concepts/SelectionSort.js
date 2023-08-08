ad9e685a3d9d8ad280734f653cb4190ffd1f2ad1
miltonhuynh <miltonhuynh@gmail.com>
2023-08-07 22:25:38 -0700

    added uL tag to selection sort

--- app/src/Concepts/SelectionSort.js
+++ app/src/Concepts/SelectionSort.js
@@ -26,26 +26,30 @@ export default function SelectionSort() {
     <div class="Concept_page">
       <h1>Selection sort</h1>
       <main>
-        <section><li>Selection sort works by repeatedly finding the smallest element in the array and putting it at the beginning.</li></section>
-        <section>
-          <li>
-            The algorithm mains two subarrays in any given array:
-            <ul>
-              <li>1) The subarray that is already sorted</li>
-              <li>2) The remaining subarray which is unsorted</li>
-            </ul>
-          </li>
-        </section>
-        <section><li>This method of sorting has a time complexity of n{n_squared}.</li></section>
-        <section><li>In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray. </li></section>
-        <div class="font-l">JavaScript Implementation</div>
-        <section>
-          <pre>
-            <code>
-              {selection}
-            </code>
-          </pre>
-        </section>
+        <ul>
+          <section>
+            <li>Selection sort works by repeatedly finding the smallest element in the array and putting it at the beginning.</li>
+          </section>
+          <section>
+            <li>
+              The algorithm mains two subarrays in any given array:
+              <ul>
+                <li>1) The subarray that is already sorted</li>
+                <li>2) The remaining subarray which is unsorted</li>
+              </ul>
+            </li>
+          </section>
+          <section><li>This method of sorting has a time complexity of n{n_squared}.</li></section>
+          <section><li>In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray. </li></section>
+          <div class="font-l">JavaScript Implementation</div>
+          <section>
+            <pre>
+              <code>
+                {selection}
+              </code>
+            </pre>
+          </section>
+        </ul>
       </main>
     </div>
   )
